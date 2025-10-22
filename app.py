0108463c6eca1a3e19232a1782278f379ba60d25
javier <estudiopena2017@gmail.com>
2025-10-22 20:20:51 -0300

    Primer commit

--- app.py
+++ app.py
@@ -388,3 +388,13 @@ if __name__ == '__main__':
 
 
 
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hola desde Render"
+
+if __name__ == '__main__':
+    app.run()
